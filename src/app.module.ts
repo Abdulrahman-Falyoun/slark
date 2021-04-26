90b72aa8e6b2fd7fb353c51fe7a0ac1ff5ef633e
AbdulrahmanFalyounST <abd.falyoun@scandinavia.com>
2021-04-26 17:03:11 +0300

    Fixing bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import {Module} from '@nestjs/common';
+import {Controller, Get, Module, Req, Res} from '@nestjs/common';
 import {MongooseInitializerModule} from './mongoose/mongoose-initializer.module';
 import {ConfigModule} from './config/config.module';
 import {MailModule} from './mail/mail.module';
@@ -8,9 +8,31 @@ import {WorkspaceModule} from './workspace/workspace.module';
 import {SpaceModule} from './space/space.module';
 import {ListModule} from './list/list.module';
 import {TaskModule} from './task/task.module';
-import { RoleModule } from './role/role.module';
+import {RoleModule} from './role/role.module';
+import {FileHandler} from "./utils/file-system-handler";
+
+@Controller()
+class globalController {
+    @Get()
+    async getMainPage(@Req() req, @Res() res) {
+        if (req.headers.host === "nest-slark.herokuapp.com") {
+            const verificationPage = await FileHandler.readFileAsync(
+                "src/public/pages/landing-page.html"
+            );
+            res.writeHead(200, {
+                "Content-Type": "text/html",
+            });
+            res.write(verificationPage);
+            res.end();
+            return res.status(200).json({message: "App started successfully"});
+        }
+    }
+}
 
 @Module({
+    controllers: [
+        globalController
+    ],
     imports: [
         ConfigModule.register({folder: 'config'}),
         MongooseInitializerModule,
