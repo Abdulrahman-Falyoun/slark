60fb1c857627094f564d87ff1c17bc862d2ff9b6
AbdulrahmanFalyounScand <abd.falyoun@scandinaviatech.com>
2021-06-27 22:17:53 +0300

    [REFACTORING]

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,33 +1,37 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, Req, Res, UseGuards, Put} from '@nestjs/common';
-import {TaskService} from './task.service';
-import {CreateTaskDto} from './dto/create-task.dto';
-import {operationsCodes} from "../utils/operation-codes";
-import {JwtAuthGuard} from "../authentication/jwt-auth.guard";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Res,
+  UseGuards,
+  Put,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { JwtAuthGuard } from '../authentication/jwt-auth.guard';
 
 @Controller('task')
 @UseGuards(JwtAuthGuard)
 export class TaskController {
-    constructor(private readonly taskService: TaskService) {
-    }
+  constructor(private readonly taskService: TaskService) {}
 
-    @Post()
-    async createTask(@Res() res, @Body() createTaskDto: CreateTaskDto) {
-        const response = await this.taskService.createTask(createTaskDto);
-        return res
-            .status(operationsCodes.getResponseCode(response.code))
-            .json(response);
-    }
+  @Post()
+  createTask(@Res() res, @Body() createTaskDto: CreateTaskDto) {
+    return this.taskService.createTask(createTaskDto);
+  }
 
-    @Delete('/:id')
-    async deleteTask(@Res() res, @Param('id') id: string) {
-        const response = await this.taskService.deleteTask(id);
-        return res.status(operationsCodes.getResponseCode(response.code)).json(response);
-    }
+  @Delete('/:id')
+  deleteTask(@Res() res, @Param('id') id: string) {
+    return this.taskService.deleteTask(id);
+  }
 
-    @Put('/:id')
-    async updateTask(@Res() res, @Req() req, @Param('id') id: string) {
-        const response = await this.taskService.updateTask(id, req.body);
-        return res.status(operationsCodes.getResponseCode(response.code)).json(response);
-    }
-
+  @Put('/:id')
+  updateTask(@Res() res, @Req() req, @Param('id') id: string) {
+    return this.taskService.updateTask(id, req.body);
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,148 +1,90 @@
-import {Injectable} from '@nestjs/common';
-import {CreateTaskDto} from './dto/create-task.dto';
-import {InjectModel} from "@nestjs/mongoose";
-import {Task} from "./entities/task.entity";
-import {ClientSession, Model} from "mongoose";
-import {SLARK_TASK} from "../utils/schema-names";
-import {operationsCodes} from "../utils/operation-codes";
-import {wrapFunctionWithinTransaction} from "../utils/transaction-initializer";
-import {ListService} from "../list/list.service";
-import {UserUtilsService} from "../user/user-utils.service";
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './entities/task.entity';
+import { ClientSession, FilterQuery, Model } from 'mongoose';
+import { SLARK_TASK } from '../utils/schema-names';
+import { withTransaction } from '../utils/transaction-initializer';
+import { ListService } from '../list/list.service';
+import { UserUtilsService } from '../user/user-utils.service';
+import { MongoError } from 'mongodb';
 
 @Injectable()
 export class TaskService {
-    constructor(
-        // @ts-ignore
-        @InjectModel(SLARK_TASK) private readonly taskModel: Model<Task>,
-        private listService: ListService,
-        private userUtilsService: UserUtilsService
-    ) {
-    }
+  constructor(
+    // @ts-ignore
+    @InjectModel(SLARK_TASK) private readonly taskModel: Model<Task>,
+    private listService: ListService,
+    private userUtilsService: UserUtilsService,
+  ) {}
 
+  async createTask(task: CreateTaskDto) {
+    const taskInstance = new this.taskModel(task);
+    return await withTransaction(
+      this.taskModel,
+      async (session: ClientSession) => {
+        // Creating new task
+        const t: Task = await taskInstance.save({ session });
 
-    async createTask(task: CreateTaskDto) {
-        if (!task._list) {
-            return {
-                code: operationsCodes.MISSING_DATA,
-                message: `There are some missing data, required fields (_list), received undefined for one or both of them`,
-            };
-        }
-        const taskInstance = new this.taskModel(task);
-        return await wrapFunctionWithinTransaction(
-            this.taskModel,
-            async (session: ClientSession) => {
-                // Creating new task
-                const t: Task = await taskInstance.save({session});
-
-                // Updating list
-                await this.listService.updateList(
-                    t._list,
-                    {$push: {_tasks: t}},
-                    {session}
-                );
-            },
-            {
-                task: taskInstance,
-                message: "Task created successfully.!",
-            },
-            {
-                message: "Could not create task ",
-            }
+        // Updating list
+        await this.listService.updateList(
+          t._list,
+          { $push: { _tasks: t } },
+          { session },
         );
-    }
+      },
+    );
+  }
 
-    async updateTask(taskId, updatedData) {
-        if (!taskId) {
-            return {
-                code: operationsCodes.MISSING_DATA,
-                message: "You should provide a valid task id: received: " + taskId,
-            };
+  async updateTask(taskId, updatedData) {
+    const task: Task = await this.findOne({ _id: taskId });
+    return await withTransaction(
+      this.taskModel,
+      async (session: ClientSession) => {
+        if (
+          updatedData?._assignedUsers &&
+          updatedData?._assignedUsers.length > 0
+        ) {
+          const oldAssignedUsers = task._assignedUsers;
+          const newAssignUsers = updatedData?._assignedUsers;
+          let usersMustBeAssignedThisTask = newAssignUsers.filter(
+            (item) => oldAssignedUsers.indexOf(item) < 0,
+          );
+          await this.userUtilsService.updateMany(
+            { _id: usersMustBeAssignedThisTask },
+            { $push: { _tasks: task._id } },
+            { session },
+          );
+          let usersShouldBeOmittedFromAssignUsersToThisTask = oldAssignedUsers.filter(
+            (item) => newAssignUsers.indexOf(item) < 0,
+          );
+          await this.userUtilsService.updateMany(
+            { _id: usersShouldBeOmittedFromAssignUsersToThisTask },
+            { $pullAll: { _tasks: [task._id] } },
+            { session },
+          );
         }
-        if (!updatedData) {
-            return {
-                code: operationsCodes.MISSING_DATA,
-                message: `Nothing to update: received: ${JSON.stringify(updatedData)}`,
-            };
-        }
-        const task: Task = await this.taskModel.findById({_id: taskId});
-        if (!task) {
-            return {
-                message: `There's no task associated with ID: ${taskId}`,
-                code: operationsCodes.UNAVAILABLE_RESOURCE,
-            };
-        }
-
-        return await wrapFunctionWithinTransaction(
-            this.taskModel,
-            async (session: ClientSession) => {
-                if (
-                    updatedData?._assignedUsers &&
-                    updatedData?._assignedUsers.length > 0
-                ) {
-                    const oldAssignedUsers = task._assignedUsers;
-                    const newAssignUsers = updatedData?._assignedUsers;
-                    let usersMustBeAssignedThisTask = newAssignUsers.filter(
-                        (item) => oldAssignedUsers.indexOf(item) < 0
-                    );
-                    await this.userUtilsService.updateMany(
-                        {_id: usersMustBeAssignedThisTask},
-                        {$push: {_tasks: task._id}},
-                        {session}
-                    );
-                    let usersShouldBeOmittedFromAssignUsersToThisTask = oldAssignedUsers.filter(
-                        (item) => newAssignUsers.indexOf(item) < 0
-                    );
-                    await this.userUtilsService.updateMany(
-                        {_id: usersShouldBeOmittedFromAssignUsersToThisTask},
-                        {$pullAll: {_tasks: [task._id]}},
-                        {session}
-                    );
-                }
-                await this.taskModel.updateOne(
-                    {_id: taskId},
-                    {$set: updatedData},
-                    {session}
-                );
-            },
-            {
-                code: operationsCodes.SUCCESS,
-                message: "Task updated successful",
-            },
-            {
-                code: operationsCodes.DATABASE_ERROR,
-                message: "Could not update task",
-            }
+        await this.taskModel.updateOne(
+          { _id: taskId },
+          { $set: updatedData },
+          { session },
         );
-    }
+      },
+    );
+  }
 
-    async deleteTask(taskId) {
-        if (!taskId) {
-            return {
-                code: operationsCodes.MISSING_DATA,
-                message: "You should provide a valid task id: received: " + taskId,
-            };
-        }
-        try {
-            const task: Task = await this.taskModel.findById({_id: taskId});
-            if (!task) {
-                return {
-                    code: operationsCodes.MISSING_DATA,
-                    message: "Task does not exist",
-                };
-            }
-            await task.deleteOne();
-            return {
-                code: operationsCodes.SUCCESS,
-                message: "Task deleted successfully",
-            };
-        } catch (e) {
-            console.log("Error [task.service.ts]: ", e.message || e);
-            return {
-                code: operationsCodes.DATABASE_ERROR,
-                message: "Could not remove task",
-                e,
-            };
-        }
-    }
-
+  async findOne(filterQuery: FilterQuery<Task>) {
+    return await this.taskModel.findOne(filterQuery).then((r) => {
+      if (!r) {
+        throw new MongoError({
+          message: `Task not found`,
+        });
+      }
+      return r;
+    });
+  }
+  async deleteTask(taskId) {
+    const task: Task = await this.findOne({ _id: taskId });
+    await task.deleteOne();
+  }
 }
