5726879404687ed6b7ac79e21a4171497999ce1a
AbdulrahmanFalyounScand <abd.falyoun@scandinaviatech.com>
2021-07-02 14:01:09 +0300

    [ADD]: file upload

--- src/workspace/workspace.model.ts
+++ src/workspace/workspace.model.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { FileModel } from '../../libs/file-upload/src';
+import { SLARK_FILE } from '../utils/schema-names';
 
 @Schema({
   timestamps: true,
@@ -9,6 +10,7 @@ export class Workspace extends Document {
   @Prop() name: string;
   @Prop({
     type: Types.ObjectId,
+    ref: SLARK_FILE,
   })
   image: FileModel;
 }
